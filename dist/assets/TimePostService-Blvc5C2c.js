e5ebd099cdef3a37c56bf4639a65c429b6a6e76f
alfredo7792 <alfredo7791@gmail.com>
2026-01-15 22:56:55 -0500

    feat: update dist

---
+++ dist/assets/TimePostService-Blvc5C2c.js
@@ -0,0 +1,1 @@
+import{e as o}from"./index-C4i4XpOx.js";const s="https://api.ricardopalma.ingpe.com/api",e={async storeTimePost(t){return await o.post(`${s}/time-post/store`,t)}};export{e as T};
--- dist/assets/TimePostService-Bwyo2k1Q.js
+++
@@ -1,1 +0,0 @@
-import{e as o}from"./index-Bn2IkI67.js";const s="https://api.ricardopalma.ingpe.com/api",e={async storeTimePost(t){return await o.post(`${s}/time-post/store`,t)}};export{e as T};
